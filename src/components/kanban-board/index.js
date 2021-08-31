7b8d225d84f8627670ec98c10cb35fe311a20f61
yogesh <yogesh.bhaware17@gmail.com>
2021-08-31 15:31:20 -0700

    forward and backward button funcyionality

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -9,135 +9,132 @@ export default class KanbanBoard extends Component {
     };
     this.stagesNames = [
       {
-        title: 'Backlog', tasks: [
-          { name: '1', stage: 0 },
-          { name: '2', stage: 0 },
-        ]
+        title: "Backlog",
+        tasks: [
+          { name: "1", stage: 0 },
+          { name: "2", stage: 0 },
+        ],
       },
-      { title: 'To Do', tasks: [] },
-      { title: 'Ongoing', tasks: [] },
-      { title: 'Done', tasks: [] },
+      { title: "To Do", tasks: [] },
+      { title: "Ongoing", tasks: [] },
+      { title: "Done", tasks: [] },
     ];
     this.handleCreateTask = this.handleCreateTask.bind(this);
     this.handleForward = this.handleForward.bind(this);
     this.handleBackward = this.handleBackward.bind(this);
-
   }
 
   handleCreateTask = (e) => {
     this.setState({
-      fname: e.target.value
-    })
-  }
+      fname: e.target.value,
+    });
+  };
 
   handleSubmit = (e) => {
-    if(this.state.fname === ""){
-      alert("Please Enter the Task ")
-    }else{
+    if (this.state.fname === "") {
+      alert("Please Enter the Task ");
+    } else {
       let user = {
         name: this.state.fname,
-        stage: 0
-      }
+        stage: 0,
+      };
       var data = this.stagesNames;
-      let array = this.stagesNames[0].tasks
-      array.push(user)
+      let array = this.stagesNames[0].tasks;
+      array.push(user);
       var obj = {
-        title: 'Backlog',
-        tasks: array
-      }
+        title: "Backlog",
+        tasks: array,
+      };
       data.splice(0, 1, obj);
-      this.setState({ stagesNames: data ,fname:""})
-
+      this.setState({ stagesNames: data, fname: "" });
     }
-   
-
-  }
+  };
 
   handleForward = (e, i, index, task) => {
     e.preventDefault();
-    const dragArray = []
+    const dragArray = [];
     this.stagesNames.forEach((item, id) => {
-      var newCards = item.tasks
+      var newCards = item.tasks;
       if (id == i + 1) {
-        newCards.push(task)
+        newCards.push(task);
       }
       if (id == i) {
-        var deleteCard = newCards.splice(index, 1)
-        newCards = deleteCard
+        var deleteCard = newCards.splice(index, 1);
+        newCards = deleteCard;
       }
       var obj = {
         title: item.title,
-        tasks: newCards
-      }
-      dragArray.push(obj)
-    })
-    this.setState({ stagesNames: dragArray })
+        tasks: newCards,
+      };
+      dragArray.push(obj);
+    });
+    this.setState({ stagesNames: dragArray });
+  };
 
-  }
-
-
-  handleDelete = (e,stage , index, task) =>{
-
+  handleDelete = (e, stage, index, task) => {
     e.preventDefault();
     let stageN = this.state.stagesNames;
-    console.log("this is 4 array",stageN)
-    stageN.map((item,i) => {
-      if(i === stage){
-
+    console.log("this is 4 array", stageN);
+    stageN.map((item, i) => {
+      if (i === stage) {
         let tasks = item.tasks;
-        item.tasks.splice(index,1)
-         }
-      
-    })
+        item.tasks.splice(index, 1);
+      }
+    });
     this.setState({
-      stagesNames:stageN
-    })
-    }
-
-
-
+      stagesNames: stageN,
+    });
+  };
 
   handleBackward = (e, i, index, task) => {
     e.preventDefault();
-    const dragArray = []
+    const dragArray = [];
     this.stagesNames.forEach((item, id) => {
-      var newCards = item.tasks
+      var newCards = item.tasks;
       if (id == i - 1) {
-        newCards.push(task)
+        newCards.push(task);
       }
       if (id == i) {
-        var deleteCard = newCards.splice(index, 1)
-        newCards = deleteCard
+        var deleteCard = newCards.splice(index, 1);
+        newCards = deleteCard;
       }
       var obj = {
         title: item.title,
-        tasks: newCards
-      }
-      dragArray.push(obj)
-    })
-    this.setState({ stagesNames: dragArray })
-  }
+        tasks: newCards,
+      };
+      dragArray.push(obj);
+    });
+    this.setState({ stagesNames: dragArray });
+  };
 
   render() {
     const { tasks } = this.state;
 
-    let stagesTasks = this.stagesNames
+    let stagesTasks = this.stagesNames;
 
     console.log("Stage tasks : ", stagesTasks);
-
 
     return (
       <div className="mt-20 layout-column justify-content-center align-items-center">
         <section className="mt-50 layout-row align-items-center justify-content-center">
-          <input id="create-task-input" type="text" className="large" placeholder="New task name"
+          <input
+            id="create-task-input"
+            type="text"
+            className="large"
+            placeholder="New task name"
             data-testid="create-task-input"
             name="name"
             value={this.state.fname}
             onChange={this.handleCreateTask}
           />
-          <button type="submit" className="ml-30" data-testid="create-task-button"
+          <button
+            type="submit"
+            className="ml-30"
+            data-testid="create-task-button"
             onClick={(e) => this.handleSubmit(e)}
-          >Create task</button>
+          >
+            Create task
+          </button>
         </section>
 
         <div className="mt-50 layout-row">
@@ -148,33 +145,75 @@ export default class KanbanBoard extends Component {
                 <div className="card-text">
                   <h4>{item.title}</h4>
                   <ul className="styled mt-50" data-testid={`stage-${i}`}>
-                    {item.tasks?.length > 0 && item.tasks.map((task, index) => {
-                      return <li className="slide-up-fade-in" key={`${i}${index}`}>
-                        <div className="li-content layout-row justify-content-between align-items-center">
-                          <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
-                          <div className="icons">
-                            <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
-                              <i className="material-icons"
-                                onClick={(e) => this.handleBackward(e, i, index, task)}
-                              >arrow_back</i>
-                            </button>
-                            <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}
-                              onClick={(e) => this.handleForward(e, i, index, task)}
-                            >
-                              <i className="material-icons">arrow_forward</i>
-                            </button>
-                            <button className="icon-only danger x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-delete`}
-                            onClick={(e)=>this.handleDelete(e,i,index,task)}>
-                              <i className="material-icons">delete</i>
-                            </button>
-                          </div>
-                        </div>
-                      </li>
-                    })}
+                    {item.tasks?.length > 0 &&
+                      item.tasks.map((task, index) => {
+                        return (
+                          <li className="slide-up-fade-in" key={`${i}${index}`}>
+                            <div className="li-content layout-row justify-content-between align-items-center">
+                              <span
+                                data-testid={`${task.name
+                                  .split(" ")
+                                  .join("-")}-name`}
+                              >
+                                {task.name}
+                              </span>
+                              <div className="icons">
+                                {i === 0 ? (
+                                  ""
+                                ) : (
+                                  <button
+                                    className="icon-only x-small mx-2"
+                                    data-testid={`${task.name
+                                      .split(" ")
+                                      .join("-")}-back`}
+                                  >
+                                    <i
+                                      className="material-icons"
+                                      onClick={(e) =>
+                                        this.handleBackward(e, i, index, task)
+                                      }
+                                    >
+                                      arrow_back
+                                    </i>
+                                  </button>
+                                )}
+                                {i === 3 ? (
+                                  ""
+                                ) : (
+                                  <button
+                                    className="icon-only x-small mx-2"
+                                    data-testid={`${task.name
+                                      .split(" ")
+                                      .join("-")}-forward`}
+                                    onClick={(e) =>
+                                      this.handleForward(e, i, index, task)
+                                    }
+                                  >
+                                    <i className="material-icons">
+                                      arrow_forward
+                                    </i>
+                                  </button>
+                                )}
+                                <button
+                                  className="icon-only danger x-small mx-2"
+                                  data-testid={`${task.name
+                                    .split(" ")
+                                    .join("-")}-delete`}
+                                  onClick={(e) =>
+                                    this.handleDelete(e, i, index, task)
+                                  }
+                                >
+                                  <i className="material-icons">delete</i>
+                                </button>
+                              </div>
+                            </div>
+                          </li>
+                        );
+                      })}
                   </ul>
                 </div>
               </div>
-            )
+            );
           })}
         </div>
       </div>
